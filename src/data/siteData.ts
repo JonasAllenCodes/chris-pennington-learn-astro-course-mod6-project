5f915c908d6a9ced3511de468f6fdadac901c435
Jonas Allen <Webmaster@JonasAllen.Codes>
2024-03-19 17:19:53 -0700

    BUILD Layout Setup
    Setup layout, styles, global data, and favicon.

---
+++ src/data/siteData.ts
@@ -0,0 +1,4 @@
+export const baseData = {
+    title: "Katie Haus Portfolio Site",
+    description: "Welcome to my portfolio site! I'm a self-taught designer and developer obsesed wit quality.",
+}
